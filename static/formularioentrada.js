6106b6f7e6715f9a0df0cc67687c58a1ee272f31
dopaoj150-cell <dopaoj150@gmail.com>
2026-08-17 10:32:42 -0300

    feat: tarefa do aluno A

--- static/formularioentrada.js
+++ static/formularioentrada.js
@@ -1,1 +1,12 @@
+const frases = [
+  "Você é brabo!",
+  "O código está funcionando! 🚀",
+  "JavaScript é vida.",
+  "Erro? Que erro? 👀",
+  "Hoje é um ótimo dia para programar."
+];
 
+const numero = Math.floor(Math.random() * frases.length);
+
+console.log("🎲 Mensagem aleatória:");
+console.log(frases[numero]);
